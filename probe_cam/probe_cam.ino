4e3a08263d66bae459fbe5d021df4dbc7eb46bd0
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2024-05-29 03:07:54 -0300

    #1 - Create CAM code

--- probe_cam/probe_cam.ino
+++ probe_cam/probe_cam.ino
@@ -2,19 +2,64 @@
 //#define ISDEBUG 1
 //#define ISPRETTYDEBUG 1
 
-TaskHandle_t ParseCameraTaskHandle;
-
 #include "Wire.h"
 #include "Wifi.h"
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+#include "esp_camera.h"
 
-enum class PARSE_ERROR_CODES {
-  PARSE_SUCCESS,
-  PARSE_DATA_NOT_APPENDED,
-  PARSE_DATA_NOT_PRINTED
+//WROVER-KIT PIN Map
+#define CAM_PIN_PWDN    -1
+#define CAM_PIN_RESET   -1
+#define CAM_PIN_XCLK    21
+#define CAM_PIN_SIOD    26
+#define CAM_PIN_SIOC    27
+#define CAM_PIN_D7      35
+#define CAM_PIN_D6      34
+#define CAM_PIN_D5      39
+#define CAM_PIN_D4      36
+#define CAM_PIN_D3      19
+#define CAM_PIN_D2      18
+#define CAM_PIN_D1       5
+#define CAM_PIN_D0       4
+#define CAM_PIN_VSYNC   25
+#define CAM_PIN_HREF    23
+#define CAM_PIN_PCLK    22
+
+#define CAM_XCLK_FREQ 20000000 //EXPERIMENTAL: Set to 16MHz on ESP32-S2 or ESP32-S3 to enable EDMA mode
+#define JPEG_QUALITY 5 //Originaly is 12
+#define FB_COUNT 1 //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+
+static camera_config_t camera_config = {
+  .pin_pwdn  = CAM_PIN_PWDN,
+  .pin_reset = CAM_PIN_RESET,
+  .pin_xclk = CAM_PIN_XCLK,
+  .pin_sccb_sda = CAM_PIN_SIOD,
+  .pin_sccb_scl = CAM_PIN_SIOC,
+
+  .pin_d7 = CAM_PIN_D7,
+  .pin_d6 = CAM_PIN_D6,
+  .pin_d5 = CAM_PIN_D5,
+  .pin_d4 = CAM_PIN_D4,
+  .pin_d3 = CAM_PIN_D3,
+  .pin_d2 = CAM_PIN_D2,
+  .pin_d1 = CAM_PIN_D1,
+  .pin_d0 = CAM_PIN_D0,
+  .pin_vsync = CAM_PIN_VSYNC,
+  .pin_href = CAM_PIN_HREF,
+  .pin_pclk = CAM_PIN_PCLK,
+
+  .xclk_freq_hz = CAM_XCLK_FREQ,
+  .ledc_timer = LEDC_TIMER_0,
+  .ledc_channel = LEDC_CHANNEL_0,
+
+  .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+  .frame_size = FRAMESIZE_UXGA, //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+
+  .jpeg_quality = JPEG_QUALITY, //0-63, for OV series camera sensors, lower number means higher quality
+  .fb_count = FB_COUNT, 
+  .grab_mode = CAMERA_GRAB_WHEN_EMPTY //CAMERA_GRAB_LATEST. Sets when buffers should be filled
 };
-using parse_error_t = PARSE_ERROR_CODES;
 
 //Definições referentes ao Wifi:
 #define WLAN_SSID "LabMicros"
@@ -27,18 +72,16 @@ WiFiClient client;
 #define MQTT_USERNAME "mqtt"
 #define MQTT_KEY "mqtt_123_abc"
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
+Adafruit_MQTT_Subscribe takephoto = Adafruit_MQTT_Subscribe(&mqtt, "sharp_probe/photo_request");
 
-void parse_Camera (*pvParameters) {
-  for( ; ; ) {
-    variável imagem; //ver na biblioteca de Camera da ESP32
 
-    if(MQTT.read(tópico---->pictures) == true) {
-      Camera.tirar_foto() //ver na biblioteca de Camera ESP32
+void parse_Camera () {
+  camera_fb_t * fb = esp_camera_fb_get();
+
+  mqtt.publish("sharp_probe/send_photo", fb->buf, fb->len);
   
-      //MQTT data transmission
-      MQTT.attach(tópico---->pictures); //.attach para enviar uma foto e .publish para enviar texto, como foi nas outras funções (fernandao disse de cabeca, pesquisar melhor)
-    }
-  }
+  //return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
 }
 
 void setup() {
@@ -59,21 +102,23 @@ void setup() {
 
   setup_Camera(); 
 
-  UBaseType_t uxHighWaterMarkParseCamera = 100 * configMINIMAL_STACK_SIZE;
+  takephoto.setCallback(parse_Camera);
 
-  if ((xTaskCreate(parse_Camera, "Parse Camera", uxHighWaterMarkParseCamera, NULL, 5, &ParseCameraTaskHandle)) == pdPASS) {
-    Serial.println("Tarefa 'Parse Camera' criada com sucesso!\n");
-  }
+  mqtt.subscribe(&takephoto);
 
   Serial.println("Setup da sonda concluído!");
 }
 
 void loop() {
-
+ 
 }
 
 void setup_Camera() {
-
+  esp_err_t err = esp_camera_init(&camera_config);
+  if (err != ESP_OK) {
+    ESP_LOGE(TAG, "Camera Init Failed");
+    return err;
+  }
 }
 
 void MQTT_connect() {

5c8bd2496df799952aa6724c9985e568ea2ade02
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2024-05-29 01:57:54 -0300

    #1 - Remove conflicting SPI code that will not be used

--- probe/NEO8M/platform/arch/platform_arduino.h
+++ probe/NEO8M/platform/arch/platform_arduino.h
@@ -163,65 +163,6 @@ EXPORT void gpio_toggle(gpio_pin_t pin) {
 }
 #endif
 
-/**
- * Essa macro só é definida se o SPI for ser utilizado
- *
- *
- *
- *
- *
- *
- *
- */
-#ifdef _SPI_H_INCLUDED
-
-/**
- *
- * Define o tipo da interface spi
- * Utilizado para compatibilidade com outras platform
- *
- */
-typedef int spi_t;
-static spi_t fake_spi = 1;
-
-typedef struct {
-  spi_t *spi;
-  gpio_pin_t pin;
-} spi_device_t;
-//#include <SPI.h>
-
-/**
- * Inicializa a biblioteca SPI
- * Configura os pinos de SCK, MOSI e SS como outputs
- * Pulling SCK e MOSI como low e SS como high
- *
- */
-// SPI.begin();
-
-EXPORT error_t spi_transceive(spi_device_t device, buffer_view_t rx_buffer,
-                              buffer_view_t tx_buffer) {
-
-  if (tx_buffer.size != rx_buffer.size) {
-    return 1;
-  }
-  rx_buffer.data = SPI.transfer(tx_buffer.data); // receive rx and transmit tx
-  error_t error = rx_buffer;
-  return error;
-}
-
-EXPORT error_t spi_receive(spi_device_t device, buffer_view_t buffer_view) {
-  buffer_view.data = SPI.transfer(0x00); // envia um 0 para receber os dados
-  error_t error = buffer_view;
-  return error;
-}
-
-EXPORT error_t spi_transmit(spi_device_t device, buffer_view_t buffer_view) {
-  error_t error = SPI.transfer(buffer_view.data);
-  return error;
-}
-
-#endif
-
 /***
  * MODULO UART
  *

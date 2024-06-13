2d3c8fe8010fb42a6636ac4c1262ddb1a3083a62
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2024-06-13 00:11:31 -0300

    #1 - Fix mqtt subscription bug and make some code improvements

--- probe_cam/probe_cam.ino
+++ probe_cam/probe_cam.ino
@@ -7,55 +7,42 @@
 #include "Adafruit_MQTT_Client.h"
 #include "esp_camera.h"
 
+
 //WROVER-KIT PIN Map
-#define CAM_PIN_PWDN    -1
-#define CAM_PIN_RESET   -1
-#define CAM_PIN_XCLK    21
-#define CAM_PIN_SIOD    26
-#define CAM_PIN_SIOC    27
-#define CAM_PIN_D7      35
-#define CAM_PIN_D6      34
-#define CAM_PIN_D5      39
-#define CAM_PIN_D4      36
-#define CAM_PIN_D3      19
-#define CAM_PIN_D2      18
-#define CAM_PIN_D1       5
-#define CAM_PIN_D0       4
-#define CAM_PIN_VSYNC   25
-#define CAM_PIN_HREF    23
-#define CAM_PIN_PCLK    22
+#define CAMERA_MODEL_WROVER_KIT
+#include "camera_pins.h"
 
 #define CAM_XCLK_FREQ 20000000 //EXPERIMENTAL: Set to 16MHz on ESP32-S2 or ESP32-S3 to enable EDMA mode
-#define JPEG_QUALITY 5 //Originaly is 12
-#define FB_COUNT 1 //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+#define JPEG_QUALITY 4         //Originaly is 12
+#define FB_COUNT 1             //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
 
 static camera_config_t camera_config = {
-  .pin_pwdn  = CAM_PIN_PWDN,
-  .pin_reset = CAM_PIN_RESET,
-  .pin_xclk = CAM_PIN_XCLK,
-  .pin_sccb_sda = CAM_PIN_SIOD,
-  .pin_sccb_scl = CAM_PIN_SIOC,
+  .pin_pwdn  = PWDN_GPIO_NUM,
+  .pin_reset = RESET_GPIO_NUM,
+  .pin_xclk = XCLK_GPIO_NUM,
+  .pin_sccb_sda = SIOD_GPIO_NUM,
+  .pin_sccb_scl = SIOC_GPIO_NUM,
 
-  .pin_d7 = CAM_PIN_D7,
-  .pin_d6 = CAM_PIN_D6,
-  .pin_d5 = CAM_PIN_D5,
-  .pin_d4 = CAM_PIN_D4,
-  .pin_d3 = CAM_PIN_D3,
-  .pin_d2 = CAM_PIN_D2,
-  .pin_d1 = CAM_PIN_D1,
-  .pin_d0 = CAM_PIN_D0,
-  .pin_vsync = CAM_PIN_VSYNC,
-  .pin_href = CAM_PIN_HREF,
-  .pin_pclk = CAM_PIN_PCLK,
+  .pin_d7 = Y9_GPIO_NUM,
+  .pin_d6 = Y8_GPIO_NUM,
+  .pin_d5 = Y7_GPIO_NUM,
+  .pin_d4 = Y6_GPIO_NUM,
+  .pin_d3 = Y5_GPIO_NUM,
+  .pin_d2 = Y4_GPIO_NUM,
+  .pin_d1 = Y3_GPIO_NUM,
+  .pin_d0 = Y2_GPIO_NUM,
+  .pin_vsync = VSYNC_GPIO_NUM,
+  .pin_href = HREF_GPIO_NUM,
+  .pin_pclk = PCLK_GPIO_NUM,
 
   .xclk_freq_hz = CAM_XCLK_FREQ,
   .ledc_timer = LEDC_TIMER_0,
   .ledc_channel = LEDC_CHANNEL_0,
 
-  .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
-  .frame_size = FRAMESIZE_UXGA, //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+  .pixel_format = PIXFORMAT_JPEG,     //YUV422,GRAYSCALE,RGB565,JPEG
+  .frame_size = FRAMESIZE_UXGA,       //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
 
-  .jpeg_quality = JPEG_QUALITY, //0-63, for OV series camera sensors, lower number means higher quality
+  .jpeg_quality = JPEG_QUALITY,       //0-63, for OV series camera sensors, lower number means higher quality
   .fb_count = FB_COUNT, 
   .grab_mode = CAMERA_GRAB_WHEN_EMPTY //CAMERA_GRAB_LATEST. Sets when buffers should be filled
 };
@@ -71,7 +58,7 @@ WiFiClient client;
 #define MQTT_USERNAME "mqtt"
 #define MQTT_KEY "mqtt_123_abc"
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
-Adafruit_MQTT_Subscribe takephoto = Adafruit_MQTT_Subscribe(&mqtt, "sharp_probe/photo_request");
+Adafruit_MQTT_Subscribe takephoto = Adafruit_MQTT_Subscribe(&mqtt, "sharp_probe/request_photo");
 
 
 void parse_Camera (uint32_t a) {
@@ -106,7 +93,13 @@ void setup() {
 }
 
 void loop() {
- 
+  MQTT_connect();
+
+  mqtt.processPackets(10000);
+
+  if(!mqtt.ping()) {
+    mqtt.disconnect();
+  }
 }
 
 void setup_Camera() {
